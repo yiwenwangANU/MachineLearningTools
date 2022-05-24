0625391fd507a0d584caae8e1d8b83a5ec14f2d7
yiwen <wangyiwenau@gmail.com>
2022-05-25 09:52:18 +1000

    update two methods

--- helperfunctions.py
+++ helperfunctions.py
@@ -27,9 +27,11 @@ def walk_through_dir(dir_path):
         print(f"Current directory: {dirpath}, number of sub-directories: {len(dirnames)}, "
               f"number of files: {len(filenames)}")
 
+
 import random
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+
 
 def plot_random_image(dir_path, num=3):
     """
@@ -39,9 +41,9 @@ def plot_random_image(dir_path, num=3):
     :param num: number of images that going to be plotted, default value is 3
     :return: None
     """
-    plt.figure(figsize=(7*num, 14))
+    plt.figure(figsize=(7 * num, 14))
     for i in range(num):
-        plt.subplot(1, num, i+1)
+        plt.subplot(1, num, i + 1)
         class_name = random.sample(os.listdir(dir_path), 1)[0]
         img_name = random.sample(os.listdir(os.path.join(dir_path, class_name)), 1)[0]
         img_path = os.path.join(dir_path, class_name, img_name)
@@ -49,3 +51,43 @@ def plot_random_image(dir_path, num=3):
         plt.imshow(img)
         plt.title(class_name)
         plt.axis("off")
+
+
+import datetime
+import tensorflow as tf
+
+
+def create_tensorboard_callback(dir_name, experiment_name):
+    """
+    Create the callback of the tensorboard, the experiment callback will be stored
+    under the path of dir_name + experiment_name + datetime
+
+    :param dir_name: a directory path that used to store all tensorbaard callbacks
+    :param experiment_name: the name of one experiment
+    :return: log file of tensorboard callback
+    """
+
+    log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(
+        log_dir=log_dir)
+    print(f"Saving TensorBoard log files to: {log_dir}")
+    return tensorboard_callback
+
+import numpy as np
+
+def plot_loss_curve(history):
+    """
+    plot the loss curve and accuracy curve during the model training
+
+    :param history: a history file that records loss, val_loss, accuracy and val_accuracy during the model training
+    :return: None
+    """
+    plt.figure(figsize=(20, 7))
+    plt.subplot(1, 2, 1)
+    plt.plot(np.arange(len(history.history) + 1), history.history["loss"], label="loss")
+    plt.plot(np.arange(len(history.history) + 1), history.history["val_loss"], label="val_loss")
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.plot(np.arange(len(history.history) + 1), history.history["accuracy"], label="accuracy")
+    plt.plot(np.arange(len(history.history) + 1), history.history["val_accuracy"], label="val_accuracy")
+    plt.legend()
