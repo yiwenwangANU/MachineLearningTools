0f733a7b0ddd2f59d5cb132ef945b6be3b951984
yiwen <wangyiwenau@gmail.com>
2022-05-24 15:36:37 +1000

    update

--- helperfunctions.py
+++ helperfunctions.py
@@ -26,3 +26,26 @@ def walk_through_dir(dir_path):
     for dirpath, dirnames, filenames in os.walk(dir_path):
         print(f"Current directory: {dirpath}, number of sub-directories: {len(dirnames)}, "
               f"number of files: {len(filenames)}")
+
+import random
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+def plot_random_image(dir_path, num=3):
+    """
+    Plot random image within training set or test set with their label
+
+    :param dir_path: a directory path that contains labeled images
+    :param num: number of images that going to be plotted, default value is 3
+    :return: None
+    """
+    plt.figure(figsize=(7*num, 14))
+    for i in range(num):
+        plt.subplot(1, num, i+1)
+        class_name = random.sample(os.listdir(dir_path), 1)[0]
+        img_name = random.sample(os.listdir(os.path.join(dir_path, class_name)), 1)[0]
+        img_path = os.path.join(dir_path, class_name, img_name)
+        img = mpimg.imread(img_path)
+        plt.imshow(img)
+        plt.title(class_name)
+        plt.axis("off")
